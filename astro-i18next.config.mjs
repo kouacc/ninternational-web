35374678d383b8aff8aab699ebfaf089cad80bfa
kouacc <maxrur57@gmail.com>
2024-01-18 14:42:31 +0100

    added ru localization

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
-  locales: ["en", "fr", "de", "hu"],
+  locales: ["en", "fr", "de", "hu", "ru"],
 };
