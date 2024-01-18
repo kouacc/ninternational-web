17f4482aec87e4e34f11822e21f9a533016b47f0
kouacc <maxrur57@gmail.com>
2024-01-18 22:35:23 +0100

    transition to content collection and nl localization

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection} from "astro:content";
+
+const projectsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        projectName: z.string(),
+        projectAuthor: z.string(),
+        projectLanguage: z.string(),
+        projectSystem: z.string(),
+        projectController: z.enum([z.string()).optional(),
+        projectImg: z.string(),
+        projectDate: z.date(),
+        description: z.string(),
+    })
+});
+
+export const collections = {
+    projects: projectsCollection,
+  };
