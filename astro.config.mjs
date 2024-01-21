b8598d443e4cb1997f9bacddffe5b0c5bf83257e
kouacc <maxrur57@gmail.com>
2024-01-21 11:53:03 +0100

    Netlify Rendering removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 import mdx from "@astrojs/mdx";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), astroI18next()],
-  output: 'server',
-  adapter: netlify(),
 });
