5c95701a5ef6a2789920fa1fece01d4022a5b563
kouacc <maxrur57@gmail.com>
2024-01-21 15:48:12 +0100

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), astroI18next()],
+  integrations: [
+    mdx(), 
+    astroI18next()],
+    output: "hybrid",
+    adapters: netlify()
 });
