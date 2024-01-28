eb544a9268665c5b83d0c28f6468101fb630744a
kouacc <maxrur57@gmail.com>
2024-01-28 11:54:45 +0100

    Added spanish localization and filter checkboxes placeholders

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en-US",
-  locales: ["en-US", "fr", "de", "hu", "ru", "nl", "pl", "ko", "lv"],
+  locales: ["en-US", "fr", "de", "es", "hu", "ru", "nl", "pl", "ko", "lv"],
   load: ["server"],
 };
